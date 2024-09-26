4180379ddc78d12ce77bb33d44136e10ce771ffd
Xun Li <lxfind@gmail.com>
2024-09-26 09:33:59 -0700

    Add SqlBackfill

---
+++ crates/sui-indexer/src/backfill/backfill_instances/sql_backfills/full_objects_history.sh
@@ -0,0 +1,1 @@
+sui-indexer --database-url postgres://postgres@localhost:5432/postgres run-back-fill "$1" "$2" sql "INSERT INTO full_objects_history (object_id, object_version, serialized_object) SELECT object_id, object_version, serialized_object FROM objects_history" checkpoint_sequence_number
