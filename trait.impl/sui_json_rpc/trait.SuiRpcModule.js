f4eee32a9eeb0564ac4d2fe03f30f9f6dbdc3bed
rvantonder <rvantonder@users.noreply.github.com>
2024-09-27 05:59:58 +0000

    deploy: 54ccf09ef3764b5b94082860bcba858a15ce6ab7

---
+++ trait.impl/sui_json_rpc/trait.SuiRpcModule.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"sui_indexer":[["impl SuiRpcModule for <a class=\"struct\" href=\"sui_indexer/apis/governance_api/struct.GovernanceReadApi.html\" title=\"struct sui_indexer::apis::governance_api::GovernanceReadApi\">GovernanceReadApi</a>"]],
+"sui_json_rpc":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
